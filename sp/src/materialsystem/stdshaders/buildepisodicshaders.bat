f7231252e8399e058af6c353b872aed8e56e44fc
Khaled <khaledahmed400@outlook.com>
2025-09-21 22:45:33 +0100

    Added new shader, implemented source shader editor

--- sp/src/materialsystem/stdshaders/buildepisodicshaders.bat
+++ sp/src/materialsystem/stdshaders/buildepisodicshaders.bat
@@ -5,10 +5,10 @@ rem ================================
 rem ==== MOD PATH CONFIGURATIONS ===
 
 rem == Set the absolute path to your mod's game directory here ==
-set GAMEDIR=%cd%\..\..\..\game\mod_episodic
+set GAMEDIR=C:\PROGRA~2\Steam\STEAMA~1\SOURCE~1\mapbase_episodic
 
 rem == Set the relative or absolute path to Source SDK Base 2013 Singleplayer\bin ==
-set SDKBINDIR=C:\Program Files (x86)\Steam\SteamApps\common\Source SDK Base 2013 Singleplayer\bin
+set SDKBINDIR=C:\PROGRA~2\Steam\STEAMA~1\common\SOURCE~1\bin
 
 rem ==  Set the Path to your mod's root source code ==
 rem This should already be correct, accepts relative paths only!
@@ -16,7 +16,6 @@ set SOURCEDIR=..\..
 
 rem ==== MOD PATH CONFIGURATIONS END ===
 rem ====================================
-
 
 call buildsdkshaders.bat
 
